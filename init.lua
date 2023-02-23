159896b7ff2380e6b95dc64a58520fb073c3381e
bigolbug <37874669+bigolbug@users.noreply.github.com>
2023-02-23 12:56:00 -0600

    Update01

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
 minetest.register_on_chat_message(function(name, message)
-    minetest.chat_send_all(type(name).." , Message: "..type(message))
+    --minetest.chat_send_all(name.." , Message: "..type(message))
+    local player = minetest.get_player_by_name(name)
+    minetest.chat_send_player(name, "Chatting has been removed :)")
+    return true
 end)
