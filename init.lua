a433171001b37409917630c436c24f74a006035d
bigolbug <37874669+bigolbug@users.noreply.github.com>
2023-02-23 13:02:57 -0600

    MacLean Exemption

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 minetest.register_on_chat_message(function(name, message)
     --minetest.chat_send_all(name.." , Message: "..type(message))
-    local player = minetest.get_player_by_name(name)
-    minetest.chat_send_player(name, string.rep("#",500))
-    return true
+    if name ~= "MacLean" then
+        minetest.chat_send_player(name, "Chat has been disabled "..string.rep("#",500))
+        return true
+    end
+    
 end)
