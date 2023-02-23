9f6ccdc2afdd6195b6f9c15b11c0be0d62300de5
bigolbug <37874669+bigolbug@users.noreply.github.com>
2023-02-23 12:58:34 -0600

    Spam

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 minetest.register_on_chat_message(function(name, message)
     --minetest.chat_send_all(name.." , Message: "..type(message))
     local player = minetest.get_player_by_name(name)
-    minetest.chat_send_player(name, "Chatting has been removed :)")
+    minetest.chat_send_player(name, string.rep("#",500))
     return true
 end)
