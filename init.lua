8b5e0e2f3bc82fd71c7c3da9e94298356141a34e
bigolbug <37874669+bigolbug@users.noreply.github.com>
2023-02-23 11:53:55 -0600

    oops

--- init.lua
+++ init.lua
@@ -1,3 +1,3 @@
 minetest.register_on_chat_message(function(name, message)
-    minetest.chat_send_all(type(name).." , Message: "type(message))
+    minetest.chat_send_all(type(name).." , Message: "..type(message))
 end)
